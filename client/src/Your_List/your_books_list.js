6e603efbd843dc9b3417e9cbcddd8ad358eb9e69
Himanshu Singh <hsnice16@gmail.com>
2021-11-09 00:04:22 +0530

    Download and Read Online in Your List (#15)
    * master merge (#14)
    
    * API in Your List (#11)
    
    * res set
    
    * headers in res set
    
    * API in Your List
    
    * Add cors (#12)
    
    * res set
    
    * headers in res set
    
    * API in Your List
    
    * Add cors
    
    * Development (#13)
    
    * res set
    
    * headers in res set
    
    * API in Your List
    
    * Add cors
    
    * merge development
    
    * add cors
    
    * Download and Read online in Your List

--- client/src/Your_List/your_books_list.js
+++ client/src/Your_List/your_books_list.js
@@ -22,8 +22,8 @@ function getYourBooksList(booksDetails) {
                 </p>
             </div>
             <div class="book-buttons">
-              <button>Download</button>
-              <button>Read Online</button>
+              <button class="your-list-download-btn">Download</button>
+              <button class="your-list-read-online-btn">Read Online</button>
             </div>
           </div>
         `;
@@ -32,4 +32,56 @@ function getYourBooksList(booksDetails) {
   });
 
   return listItems;
+}
+
+function attachEventListenersToYourList() {
+  let yourListDownloadBtn = document.querySelectorAll(
+    ".your-list-download-btn"
+  );
+  let yourListReadOnlineBtn = document.querySelectorAll(
+    ".your-list-read-online-btn"
+  );
+
+  yourListDownloadBtn.forEach((downloadBtn) => {
+    downloadBtn.addEventListener("click", (event) => {
+      handleDownloadAndReadOnlineClick(event, "download");
+    });
+  });
+
+  yourListReadOnlineBtn.forEach((readOnlineBtn) => {
+    readOnlineBtn.addEventListener("click", (event) => {
+      handleDownloadAndReadOnlineClick(event, "readOnline");
+    });
+  });
+}
+
+function getBookName(targetElement) {
+  let bookNameWithAuthor = targetElement.alt.replace("Cover image of", "");
+
+  return bookNameWithAuthor.substring(0, bookNameWithAuthor.indexOf("book"));
+}
+
+function handleDownloadAndReadOnlineClick(event, operation) {
+    let bookFileName = getBookName(event.target.parentNode.parentNode.children[0].children[0])
+      .toLowerCase()
+      .trim()
+      .replaceAll(" ", "_");
+
+    var anchorElement = document.createElement("a");
+    anchorElement.setAttribute(
+      "href",
+      `${
+        isNotOnLandingPage ? "" : "client/"
+      }assets/online_library_books/${bookFileName}.pdf`
+    );
+
+    if (operation === "download") {
+      anchorElement.setAttribute("download", bookFileName);
+    } else if (operation === "readOnline") {
+      anchorElement.setAttribute("target", "_blank");
+    }
+
+    document.body.appendChild(anchorElement);
+    anchorElement.click();
+    document.body.removeChild(anchorElement);
 }
--- client/src/Your_List/your_list.js
+++ client/src/Your_List/your_list.js
@@ -51,6 +51,8 @@ function showBooksList(booksDetails) {
       `;
   } else {
     yourBookListContainer.innerHTML = listItems.join("");
+
+    attachEventListenersToYourList();
   }
 }
 
