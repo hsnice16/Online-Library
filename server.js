6d8a0e588c5d23153c1df7df15e715bb8a996cd6
Himanshu Singh <hsnice16@gmail.com>
2021-11-08 14:23:30 +0530

    Add cors (#12)
    * res set
    
    * headers in res set
    
    * API in Your List
    
    * Add cors

--- server.js
+++ server.js
@@ -4,15 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Methods": ["GET", "POST"],
-  });
-
-  next();
-});
+app.use(cors());
 app.use(express.json());
 app.use("/user", require("./server/api/routes"));
 
